f08a4110ef4ff5a495bb8122f809e229632e6889
zhangqiang <tyokyo320@gmail.com>
2023-05-16 20:29:10 +0900

    🎉 init: add CLA utils

---
+++ utils/config.py
@@ -0,0 +1,8 @@
+import os
+
+# Program root dir(do not modify)
+rootPATH = os.path.dirname(os.path.abspath(__file__))
+
+# CSV predict results file path
+predict_results_path = "../output/original/InputOutput_BUon_BOon_NY15min_cells32_n421_r13d08.csv"
+
