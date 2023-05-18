8e84551e1b7c929248132635ffaaee0c73ba0b03
zhangqiang <tyokyo320@gmail.com>
2023-05-19 06:02:54 +0900

    ✨ feat: update predict

---
+++ README.md
@@ -0,0 +1,15 @@
+$$
+\begin{align}
+    e(t) = \sum^{t}_{k=1} |\bar{X}(k+1) - X(k+1)|
+\end{align}
+$$
+
+$$
+\begin{align}
+    \text{MAE} = \frac{1}{n} \sum_{i=1}^{n} \mathbf{e}_i(t)
+\end{align}
+$$
+
+| measure | pattern 1 | pattern 2 | pattern 3 |
+| :-----: | :-------: | :-------: | :-------: |
+|   MAE   |  7495.07  |  10700.0  |  7418.93  |
--- utils/config.py
+++ utils/config.py
@@ -1,7 +1,7 @@
 import os
 
 # Program root dir(do not modify)
-rootPATH = os.path.dirname(os.path.abspath(__file__))
+ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
 
 # CSV predict results file path
 predict_results_path = "../output/original/InputOutput_BUon_BOon_NY15min_cells32_n421_r13d08.csv"
@@ -9,3 +9,15 @@ predict_results_path = "../output/original/InputOutput_BUon_BOon_NY15min_cells32
 # param
 nTrial = 10
 epoch = 414915
+
+# start and end index
+start = 414819  # last day
+error_abs_end = epoch * 2
+error_abs_start = error_abs_end - 24*4
+# start = 411937 # last 1 month
+
+# inf
+predict_inf_results_path = "../output/infinity/InputOutput_BUon_BOon_NY15min_cells32_n421_r13d08_infinity.csv"
+
+# 100
+predict_100_results_path = "../output/100/InputOutput_BUon_BOon_NY15min_cells32_n421_r13d08_100.csv"
