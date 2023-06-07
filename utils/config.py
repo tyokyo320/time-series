076f396716204b41c51c5a9a086261dfd73a054f
zhangqiang <tyokyo320@gmail.com>
2023-06-07 23:19:09 +0900

    ✨ feat: add zoom_coords

--- utils/config.py
+++ utils/config.py
@@ -7,10 +7,6 @@ ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
 # test data file
 test_data_path = '../data/test.csv'
 
-# CSV predict results file path
-predict_results_path = "../output/original/InputOutput_BUon_BOon_NY15min_cells32_n421_r13d08.csv"
-cell_synapases_history_path = '../output/original/CellSynapasesHistory_BUon_BOon_NY15min_cells32_n421_r13d08.csv'
-
 # param
 nTrial = 10
 epoch = 414915
@@ -32,15 +28,22 @@ color_dict = {
 # index
 # predict: 0 - 414914 -> total 414915
 # error  : 414916 -> 829830 -> total 414915
+# zoom_coords = ((zoom_xmin, zoom_xmax), (zoom_ymin, zoom_ymax))
 #######################################################################################################################
+# All observation
 # start = 0
 # end = 414914
+#######################################################################################################################
 # first day
 # start = 0
 # end = 95
+# zoom_coords = ((75, 90), (0, 200))
+#######################################################################################################################
 # last day
 start = 414819
 end = 414914
+zoom_coords = ((414900, 414914), (50, 300))
+#######################################################################################################################
 error_abs_start = start+epoch+1
 error_abs_end = end+epoch+1
 
@@ -48,6 +51,10 @@ error_abs_end = end+epoch+1
 cell_synapases_start = 0
 
 #######################################################################################################################
+# Case 1: Original
+predict_results_path = "../output/original/InputOutput_BUon_BOon_NY15min_cells32_n421_r13d08.csv"
+cell_synapases_history_path = '../output/original/CellSynapasesHistory_BUon_BOon_NY15min_cells32_n421_r13d08.csv'
+
 # Case 2: inf
 inf_predict_results_path = "../output/infinity/InputOutput_BUon_BOon_NY15min_cells32_n421_r13d08.csv"
 inf_cell_synapases_history_path = '../output/infinity/CellSynapasesHistory_BUon_BOon_NY15min_cells32_n421_r13d08.csv'
@@ -59,3 +66,4 @@ tenpercent_cell_synapases_history_path = '../output/tenpercent/CellSynapasesHist
 # Case 4: first year as min and max
 firstyear_predict_results_path = "../output/firstyear/InputOutput_BUon_BOon_NY15min_cells32_n421_r13d08.csv"
 firstyear_cell_synapases_history_path = '../output/firstyear/CellSynapasesHistory_BUon_BOon_NY15min_cells32_n421_r13d08.csv'
+#######################################################################################################################
